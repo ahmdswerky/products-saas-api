6c2cb32192eeb4eb3ec82424da20b4fd306e8739
Ahmed Swerky <ahmdswerky@gmail.com>
2022-01-05 07:35:15 +0200

    test stage deployment

--- routes/api/website.php
+++ routes/api/website.php
@@ -1,16 +1,23 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Website\PaymentController;
 use App\Http\Controllers\Website\ProductController;
 use App\Http\Controllers\Website\MerchantController;
 use App\Http\Controllers\Website\CustomerCardController;
 use App\Http\Controllers\Website\PaymentGatewayController;
+use App\Http\Controllers\Website\MerchantBalanceController;
 use App\Http\Controllers\Website\AttachPaymentMethodController;
 use App\Http\Controllers\Website\GenerateMerchantLinkController;
-use App\Http\Controllers\Website\MerchantBalanceController;
 
 Route::apiResource('products', ProductController::class)->middleware('auth:key');
+//Route::get('products/{product}', function (Product $product) {
+//    //Route::get('products/{product}', function ($product) {
+//    return response([
+//        'product' => Product::where('slug', $product)->first(),
+//    ]);
+//});
 Route::apiResource('merchants', MerchantController::class)->middleware('auth:key');
 Route::get('merchant/balance', MerchantBalanceController::class)->middleware('auth:key');
 Route::post('customers/cards', CustomerCardController::class);
@@ -18,3 +25,5 @@ Route::post('customers/payment-method', AttachPaymentMethodController::class);
 Route::apiResource('payments', PaymentController::class)->except('destroy')->middleware('auth:key');
 Route::apiResource('payment-gateways', PaymentGatewayController::class)->only('index', 'show')->middleware('auth:key');
 Route::post('merchant/link', GenerateMerchantLinkController::class)->middleware('auth:key');
+
+//
