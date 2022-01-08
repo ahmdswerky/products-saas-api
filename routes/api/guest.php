7226faca4684c5fc176efbfa095d38f9e5f6475f
Ahmed Swerky <ahmdswerky@gmail.com>
2022-01-08 08:18:50 +0200

    test: ci/cd

--- routes/api/guest.php
+++ routes/api/guest.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Website\ContactController;
 
 Route::post('signin', [AuthController::class, 'signin'])->name('signin');
 Route::post('signup', [AuthController::class, 'signup'])->name('signup');
 Route::post('signout', [AuthController::class, 'signout'])->name('signout');
 Route::get('user', [AuthController::class, 'user'])->middleware('auth:api')->name('user');
 Route::put('user', [AuthController::class, 'update'])->middleware('auth:api')->name('user.update');
+Route::post('contact', [ContactController::class, 'store']);
