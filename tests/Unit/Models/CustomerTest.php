98fe046eeaf6435fd2bcb6ad5c22ec603ea745e8
Ahmed Swerky <ahmdswerky@gmail.com>
2022-01-15 00:46:50 +0200

    test: ci/cd

--- tests/Unit/Models/CustomerTest.php
+++ tests/Unit/Models/CustomerTest.php
@@ -4,10 +4,8 @@ use App\Models\User;
 use App\Models\PaymentGateway;
 
 it('each user have customers', function () {
-    dump(User::first()->toArray());
     $gatewaysCount = PaymentGateway::count();
     $user = User::first();
-
 
     expect($user->customers)->toHaveCount($gatewaysCount);
 });
